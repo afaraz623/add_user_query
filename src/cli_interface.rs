d2c9cd07b2889dd7e022a8ff5d4692b08738c8a9
Faraz Ahmed <afaraz623@gmail.com>
2022-12-10 23:52:12 +0500

    Figured out the bare structure of the interface

---
+++ src/cli_interface.rs
@@ -0,0 +1,145 @@
+/* 
+ *  > Add Employee Name To A Department?
+ *      Format: "Add Sally to Engineering"
+ *      :- {INPUT}
+ *    Query
+ *      > Search By Department? 
+ *          :- {INPUT}
+ *        Search By Employee Name?
+ *          :- {INPUT}
+ *        Back
+ *    Exit 
+ */
+
+use dialoguer::{theme::ColorfulTheme, Select, Input, console::Style};
+
+/*
+ * menu_selections and user_inputs are only 
+ * here to wrap the dialoguer libray code 
+ * and help this code repetition.
+ */
+fn menu_selections(theme: &ColorfulTheme, menu_items: &Vec<&str>, prompt_msg: &String) -> usize
+{
+        let selec_index = Select::with_theme(theme)
+                .with_prompt(prompt_msg)
+                .default(0)
+                .items(&menu_items[..])
+                .interact()
+                .unwrap();
+
+        selec_index
+}
+
+fn user_inputs(theme: &ColorfulTheme, prompt_message: &String) -> String
+{
+        let input: String = Input::with_theme(theme)
+        .with_prompt(prompt_message)
+        .interact_text()
+        .unwrap();
+        
+        input
+}
+
+/*
+ * This is the main menu function from 
+ * which the user can select what they 
+ * want to do.
+ */
+fn init_menu_cli(theme: &ColorfulTheme) -> usize
+{
+        let init_selections = vec![
+                "Add Employee Name To A Department?",
+                "Query Database"
+        ];
+
+        let init_message = String::from("Add/Query data from a simple program");
+
+        let menu_selec = menu_selections(&theme, &init_selections, &init_message);
+
+        menu_selec
+}
+
+/*
+ * Sub-menu function which is the result
+ * of "Add Employee Name To A Department?".
+ * Returns user entry
+ */  
+fn add_data_cli(theme: &ColorfulTheme)
+{
+        let entry_format_msg = String::from("Format: \"Add Sally to Engineering\"\n:-");
+        loop 
+        {
+                let tmp = user_inputs(theme, &entry_format_msg);
+                if tmp.to_lowercase() == "back" {break};
+        }
+}
+
+/*
+ * Sub-menu function which is the result
+ * of "Query Database". It goes one level
+ * down to help the user be more specfic 
+ * when querying.
+ */
+fn query_data_cli(theme: &ColorfulTheme)
+{
+        let query_selections = vec![
+                "By Department?",
+                "By Name?",
+                "Back"
+        ];
+
+        let query_selec_msg = String::from("Query data");
+
+        loop 
+        {
+                let tmp = menu_selections(theme, &query_selections, &query_selec_msg);
+                match tmp {
+                        0 => query_depart_cli(theme),
+                        1 => query_emply_cli(theme),
+                        2 => break,
+                        _ => println!("SOMETHING IS WRONG!!!")
+                }
+        }
+}
+
+fn query_depart_cli(theme: &ColorfulTheme) 
+{
+        let query_depart_msg = String::from("Enter department name to see which employees are in it\n:-");
+        
+        loop 
+        {
+                let tmp = user_inputs(theme, &query_depart_msg);
+                if tmp.to_lowercase() == "back" {break};
+        }
+}
+
+fn query_emply_cli(theme: &ColorfulTheme) 
+{
+        let query_emply_msg = String::from("Enter employee name to see which department they work in\n:-");
+        
+        loop 
+        {
+                let tmp = user_inputs(theme, &query_emply_msg);
+                if tmp.to_lowercase() == "back" {break};
+        }
+}
+
+pub fn cli_interface() 
+{
+        let theme = ColorfulTheme {
+                values_style: Style::new().cyan().bright(),
+                ..Default::default()
+        };
+
+        loop 
+        {
+                if init_menu_cli(&theme) == 0 
+                {
+                        add_data_cli(&theme);   
+                } 
+                else 
+                {
+                        query_data_cli(&theme);
+                }
+        }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,74 +5,23 @@
     department, sorted alphabetically.
 */
 
-/* 
-    > Add Employee Name To A Department?
-        Format: "Add Sally to Engineering"
-        :- {INPUT}
-      Query
-        > By Department? 
-            :- {INPUT}
-          By Employee Name?
-            :- {INPUT}
-*/
-
+mod cli_interface;
 
 use std::collections::HashMap;
-use dialoguer::{theme::ColorfulTheme, Select, Input, console::Style};
+use cli_interface::cli_interface;
 
-fn add_data(database: &mut HashMap<String, Vec<String>>, input: String) 
-{
+fn add_data(database: &mut HashMap<String, Vec<String>>, input: String) {
         let string_to_vec: Vec<&str> = input.split_whitespace().collect();
         // dbg!(&string_to_vec);
 
         let name = string_to_vec[1].to_owned();
         let department = string_to_vec[3].to_owned();
         database.entry(department).or_insert(Vec::new()).push(name);
+        dbg!(database);
 } 
-
-fn display_cli_menu() {
-        let theme = ColorfulTheme {
-                values_style: Style::new().cyan().bright(),
-                ..Default::default()
-        };
-
-        let init_selections = vec![
-                "Add Employee Name To A Department?",
-                "Query"
-        ];
-
-        let query_selections = vec![
-                "By Department?",
-                "By Name"
-        ];
-
-        let init_selection = Select::with_theme(&theme)
-                .with_prompt("Simple CLI program to add or query a simple database")
-                .default(0)
-                .items(&init_selections[..])
-                .interact()
-                .unwrap();
-
-        if init_selection == 0 {
-                let input: String = Input::with_theme(&theme)
-                        .with_prompt("Format: \"Add Sally to Engineering\"\n:-")
-                        .interact_text()
-                        .unwrap();
-        } else {
-            let query_selection = Select::with_theme(&theme)
-            .default(0)
-            .items(&query_selections[..])
-            .interact()
-            .unwrap();
-        }
-}
 
 fn main() 
 {
         let mut database: HashMap<String, Vec<String>> = HashMap::new();
-        let s = String::from("Add Sally to Sales");
-
-
-        add_data(&mut database, s);
-        display_cli_menu();
+        cli_interface();
 }
